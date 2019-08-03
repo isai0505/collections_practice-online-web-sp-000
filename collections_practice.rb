ab00ebd39f00b9c242b65e14c43febfde835abdb
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-03 03:01:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers_array)
+  integers_array.sort! { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(element)
+  element[1],element[2] = element[2],element[1]
+  element
+end
+
+def reverse_array(reversey)
+  reversey.reverse
+end
+
+def kesha_maker(money)
+  money.each do |word|
+    word[2] = "$"
+  end
+end
